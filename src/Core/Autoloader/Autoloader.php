29bd0a3da3f2ad7bde6e6b3fd977d1115835d28f
dhh <318149908@qq.com>
2018-02-05 16:34:39 +0800

    ok l

--- src/Core/Autoloader/Autoloader.php
+++ src/Core/Autoloader/Autoloader.php
@@ -1,22 +1,22 @@
 <?php
 namespace  Content\Core\Autoloader;
-
-spl_autoload_register("Autoloader::autoload");
+spl_autoload_register("Content\Core\Autoloader\Autoloader::autoload");
 class Autoloader
 {
     private static $autoloadPathArray = array(
-        "aliyun-php-sdk-core",
-        "aliyun-php-sdk-core/Auth",
-        "aliyun-php-sdk-core/Http",
-        "aliyun-php-sdk-core/Profile",
-        "aliyun-php-sdk-core/Regions",
-        "aliyun-php-sdk-core/Exception"
+        "Core",
+        "Core/Auth",
+        "Core/Http",
+        "Core/Profile",
+        "Core/Regions",
+        "Core/Exception"
     );
-    
+
     public static function autoload($className)
     {
         foreach (self::$autoloadPathArray as $path) {
             $file = dirname(dirname(__DIR__)).DIRECTORY_SEPARATOR.$path.DIRECTORY_SEPARATOR.$className.".php";
+
             $file = str_replace('\\', DIRECTORY_SEPARATOR, $file);
             if (is_file($file)) {
                 include_once $file;
@@ -24,7 +24,7 @@ class Autoloader
             }
         }
     }
-    
+
     public static function addAutoloadPath($path)
     {
         array_push(self::$autoloadPathArray, $path);
